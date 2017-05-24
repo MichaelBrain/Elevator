afbb1f38b63ad8cd9be5a7cdcf1841e71923070b
Henry-W <henry.white@capgemini.com>
2017-05-24 18:40:53 +0100

    Added Person superclass and Developer, Employee and Building classes.

--- src/main/java/aston/jpc/Main.java
+++ src/main/java/aston/jpc/Main.java
@@ -1,4 +1,33 @@
 package aston.jpc;
 
+import java.nio.Buffer;
+
 public class Main {
+    private Building building;
+
+    private Main() {
+        this.building = new Building();
+    }
+
+    private void createEmployees(int amount) {
+        for (int i = 0; i < amount; i++) {
+            this.building.people.add(new Employee(building.people.size() + 1));
+        }
+    }
+
+    private void createDevelopers(int amount) {
+        for (int i = 0; i < amount; i++) {
+            this.building.people.add(new Developer(building.people.size() + 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        // Start application here
+        // @todo Get input from user for number of employees and developers
+        // @todo Get input from user for p and q
+        Main app = new Main();
+        app.createEmployees(20);
+        app.createDevelopers(20);
+        System.out.println(app.building.people.toString());
+    }
 }
